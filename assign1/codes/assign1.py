987b1a74066600132879898d6b1d8c6613bba0da
gingerbread69 <ee22btech11216@iith.ac.in>
2023-05-14 21:14:14 +0530

    Third Commit

--- assign1/codes/assign1.py
+++ assign1/codes/assign1.py
@@ -1,23 +1,34 @@
-outcomes = [1, 2, 3, 4, 5, 6]
-prime = 0
-btnum = 0
-odd = 0
-def isPrime(n):
-    x = 1
-    for i in range(1, n):
-        if n % i == 0:
-            x += 1
-    if x == 2:
-        return True
-    else:
-        return False
+import numpy as np
 
-for i in outcomes:
-    if i > 2 and i < 6:
-        btnum += 1
-    if i%2 == 1:
-        odd += 1
-    if isPrime(i):
-        prime += 1
+# Simulation Size
+num_rolls = 1000000
 
-print(f"Probability of (i) getting an prime number: {prime/6}, (ii) getting a number between 2 and 6: {btnum/6}, (iii) getting an odd number: {odd/6}")
+# Probability for each number on die
+probability = np.full((6,), 1/6)
+
+# Simulating die roll
+roll = np.random.choice(np.arange(1, 7), size=num_rolls, p = probability)
+
+prob_1 = np.mean(roll == 1)
+prob_2 = np.mean(roll == 2)
+prob_3 = np.mean(roll == 3)
+prob_4 = np.mean(roll == 4)
+prob_5 = np.mean(roll == 5)
+prob_6 = np.mean(roll == 6)
+
+prob_prime = np.mean(roll == 2) + np.mean(roll == 3) + np.mean(roll == 5)
+prob_bt = np.mean(roll == 3) + np.mean(roll == 4) + np.mean(roll == 5)
+prob_odd = np.mean(roll == 1) + np.mean(roll == 3) + np.mean(roll == 5)
+
+print("Probability of the number rolled being 1: ", prob_1)
+print("Probability of the number rolled being 2: ", prob_2)
+print("Probability of the number rolled being 3: ", prob_3)
+print("Probability of the number rolled being 4: ", prob_4)
+print("Probability of the number rolled being 5: ", prob_5)
+print("Probability of the number rolled being 6: ", prob_6)
+
+
+print("Probability of the number rolled being prime: ", prob_prime)
+print("Probability of the number rolled being between 2 and 6: ", prob_bt)
+print("Probability of the number rolled being odd: ", prob_odd)
+
